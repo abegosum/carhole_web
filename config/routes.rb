0f9731201253347484132ef02710f734be359801
Aaron M. Bond <ambond@aaronmbond.com>
2018-11-25 20:19:01 -0600

    Added default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'garage_status#view'
   put 'garage_control/open_close', :defaults => { :format => 'json' }
   put 'garage_control/timer_advance', :defaults => { :format => 'json' }
   get 'garage_status/view'
